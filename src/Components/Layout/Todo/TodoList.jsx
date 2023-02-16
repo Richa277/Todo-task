d634ade282097b1cb1a9ccd1bee96f3b97003810
Richa277 <125262147+Richa277@users.noreply.github.com>
2023-02-16 14:43:35 +0530

    updated todolist file

--- src/Components/Layout/Todo/TodoList.jsx
+++ src/Components/Layout/Todo/TodoList.jsx
@@ -1,44 +1,45 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { get, set } from "../../../Services/Storage";
 import styles from "./TodoList.module.css";
 
 function TodoList() {
   const [data, setData] = useState([]);
   const [todo, setTodo] = useState({ text: "" });
-  const inputValue = useRef();
+  const handleChange = (e) => {
+    setTodo((todo) => ({
+      ...todo,
+      text: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    let { id, text } = todo;
+    e.preventDefault();
+    setData((data) => {
+      if (id) {
+        return data.map((ele) => (ele.id === id ? { ...ele, text } : ele));
+      } else {
+        return [...data, { id: Math.random() * 1000, text: todo.text }];
+      }
+    });
+  };
+  const handleEdit = (idd) => {
+    let newEDit = data.find((ele) => {
+      return ele.id === idd;
+    });
+    setTodo({ text: newEDit.text, id: newEDit.id });
+  };
+  useEffect(() => {
+    set("data", data);
+    setTodo({ text: "" });
+  }, [data]);
+
   useEffect(() => {
     const items = get("data");
     if (items) {
       setData(items);
     }
   }, []);
-  const handleSubmit = (e) => {
-    let { id, text } = todo;
-    e.preventDefault();
 
-    setData((data) => {
-      if (id) {
-        text = inputValue.current.value;
-        return data.map((ele) => (ele.id === id ? { ...ele, text } : ele));
-      } else {
-        return [
-          ...data,
-          { id: Math.random() * 1000, text: inputValue.current.value },
-        ];
-      }
-    });
-  };
-  useEffect(() => {
-    set("data", data);
-    inputValue.current.value = "";
-  }, [data]);
-  const handleEdit = (idd) => {
-    let newEDit = data.find((ele) => {
-      return ele.id === idd;
-    });
-    setTodo({ text: newEDit.text, id: newEDit.id });
-    inputValue.current.value = newEDit.text;
-  };
   return (
     <div className={styles.mainForm}>
       <form>
@@ -46,7 +47,8 @@ function TodoList() {
           type="text"
           placeholder="Add your task"
           className="input"
-          ref={inputValue}
+          onChange={handleChange}
+          value={todo.text}
         />
 
         <button onClick={handleSubmit} className={styles.submit}>
